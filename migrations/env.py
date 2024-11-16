4ec262f01b02322f84c123130c846c89567cc9b6
Rosaly-J <seohwi327@gmail.com>
2024-11-16 10:59:41 +0900

    - 파일 세팅 중

--- migrations/env.py
+++ migrations/env.py
@@ -3,12 +3,13 @@ from logging.config import fileConfig
 from config.settings import Settings
 from sqlalchemy import pool, create_engine
 from sqlalchemy.engine import Connection
-from sqlalchemy.ext.asyncio import async_engine_from_config
+from sqlalchemy.ext.asyncio import async_engine_from_config, create_async_engine
 from database.db import Base # db.py에서 Base 가져오기
-
+import warnings
 
 from alembic import context
 
+warnings.filterwarnings("ignore", category=RuntimeWarning)
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config # Alembic Config 객체
@@ -16,7 +17,8 @@ fileConfig(config.config_file_name)
 
 # 데이터베이스 URL 설정
 # .env 파일에서 가져온 PostgreSQL 연결 URL을 사용합니다.
-SQLALCHEMY_DATABASE_URL = Settings.database_url  # settings에서 불러오는 PostgreSQL 연결 URL
+settings = Settings()
+SQLALCHEMY_DATABASE_URL = settings.database_url  # settings에서 불러오는 PostgreSQL 연결 URL
 target_metadata = Base.metadata
 
 # Interpret the config file for Python logging.
@@ -61,38 +63,22 @@ def do_run_migrations(connection: Connection) -> None:
         context.run_migrations()
 
 
-async def run_async_migrations() -> None:
-    """In this scenario we need to create an Engine
-    and associate a connection with the context.
-
-    """
-
-    connectable = async_engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
-
-    async with connectable.connect() as connection:
-        await connection.run_sync(do_run_migrations)
-
-    await connectable.dispose()
+connectable = create_async_engine(SQLALCHEMY_DATABASE_URL, future=True)
 
 
 # engine 생성
-def run_migrations_online():
-    """Run migrations in 'online' mode."""
-    connectable = create_engine(SQLALCHEMY_DATABASE_URL, poolclass=pool.NullPool)
-
-    with connectable.connect() as connection:
-        context.configure(
-            connection=connection,
-            target_metadata=target_metadata,
+async def run_migrations_online():
+    async with connectable.connect() as connection:
+        await connection.run_sync(
+            lambda sync_connection: context.configure(
+                connection=sync_connection,
+                target_metadata=target_metadata,
+                render_as_batch=True, # 필요 시 활성화
+            )
         )
 
-        with context.begin_transaction():
+        async with connection.begin():
             context.run_migrations()
-
 
 
 # Alembic 실행 모드에 맞는 함수 호출
@@ -100,3 +86,5 @@ if context.is_offline_mode():
     run_migrations_offline()
 else:
     run_migrations_online()
+
+    print("ok")
